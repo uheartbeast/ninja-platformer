a1e97ad90bc5b01ae98b9820e97fdb13cfb29831
Heartbeast <heartbeast.studios@gmail.com>
2025-03-24 14:10:24 -0600

    Finished part 6

--- player.gd
+++ player.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+@export var max_speed: = 120
+@export var acceleration: = 1000
+@export var air_acceleration: = 2000
+@export var friction: = 1000
+@export var air_friction: = 500
+@export var up_gravity: = 500
+@export var down_gravity: = 600
+@export var jump_amount: = 200
+
 @onready var anchor: Node2D = $Anchor
 @onready var animation_player_upper: AnimationPlayer = $AnimationPlayerUpper
 @onready var animation_player_lower: AnimationPlayer = $AnimationPlayerLower
@@ -18,22 +27,21 @@ func _ready() -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	var x_input = Input.get_axis("ui_left", "ui_right")
+	var x_input = Input.get_axis("move_left", "move_right")
 	
-	if not is_on_floor():
-		velocity.y += 500 * delta
+	apply_gravity(delta)
 	
-	velocity.x = x_input * 80
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_amount
 	
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = -200
-	
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("attack"):
 		animation_player_upper.play("attack")
 	
 	if x_input == 0:
+		apply_friction(delta)
 		animation_player_lower.play("stand")
 	else:
+		accelerate_horizontally(x_input, delta)
 		anchor.scale.x = sign(x_input)
 		animation_player_lower.play("run")
 	
@@ -41,3 +49,20 @@ func _physics_process(delta: float) -> void:
 		animation_player_lower.play("jump")
 	
 	move_and_slide()
+
+func accelerate_horizontally(horizontal_direction: float, delta: float) -> void:
+	var acceleration_amount: = acceleration
+	if not is_on_floor(): acceleration_amount = air_acceleration
+	velocity.x = move_toward(velocity.x, max_speed * horizontal_direction, acceleration_amount * delta * abs(horizontal_direction))
+
+func apply_friction(delta) -> void:
+	var friction_amount: = friction
+	if not is_on_floor(): friction_amount = air_friction
+	velocity.x = move_toward(velocity.x, 0.0, friction_amount * delta)
+
+func apply_gravity(delta) -> void:
+	if not is_on_floor():
+		if velocity.y <= 0:
+			velocity.y += up_gravity * delta
+		else:
+			velocity.y += down_gravity * delta
